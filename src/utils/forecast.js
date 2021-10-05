bbf80c8ad961eb931275531eab155077981877d2
Heikel Khaldi <heikel.khaldi1@gmail.com>
2021-10-05 14:23:44 -0400

    initial checkin

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+ const access_key = '80c7a460080eca984383d81085484b76';
+ const url = `http://api.weatherstack.com/current?access_key=${access_key}&query=${latitude},${longitude}&units=f`;
+ request.get({ url, json: true }, (error, { body }) => {
+  if (error) {
+   callback('Unable to connect to weather service', undefined);
+  } else if (body.error) {
+   callback('Unable to find location', undefined);
+  } else {
+   callback(
+    undefined,
+    `${body.current.weather_descriptions}. It is currently ${body.current.temperature}.There is a ${body.current.precip}% chance of rain`
+   );
+  }
+ });
+};
+
+module.exports = forecast;
