792f899bfbfa6b0b186037ee17f83d3858971967
Heikel Khaldi <heikel.khaldi1@gmail.com>
2021-10-05 15:27:53 -0400

    merged conflicts

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,7 @@
 console.log('client side javascript file is loaded!');
 
-<<<<<<< HEAD
 //commented method
-=======
->>>>>>> ea61021227d110fb372a2a61fe7c7e706e2be942
+
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //  response.json().then((data) => {
 //   console.log(data);
