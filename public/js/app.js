6c8cac33b3a7dd8da8fa1b458f9bb03eb7d95f0f
HKHALDI <hkhaldi@ford.com>
2021-10-05 22:26:40 -0400

    merged with unrlated stories

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,5 @@
 console.log('client side javascript file is loaded!');
 
-//commented method
-
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //  response.json().then((data) => {
 //   console.log(data);
